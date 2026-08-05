e0d9a66f672c3e6c93531585469e83b113c18188
Matej Milankov <matej.milankov@gmail.com>
2026-08-05 17:31:32 +0200

    Added language list

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { languages } from './languages'
 
 function App() {
 
@@ -8,9 +9,26 @@ function App() {
         <h1>Assembly: Endgame</h1>
         <p>Guess the word within 8 attempts to keep the programming world safe from Assembly!</p>
       </header>
+
       <section className='game-status'>
         <h2>You win!</h2>
         <p>Well done!</p>
+      </section>
+
+      <section className='language-chips'>
+        {languages.map(lang => {
+          const styles = {
+            backgroundColor: lang.backgroundColor,
+            color: lang.color
+          }
+          return (
+            <span className='chip' 
+                  style={styles}
+                  key={lang.name}>
+              {lang.name}
+            </span>
+          )
+        })}
       </section>
     </main>
   )
